a72f31480f8cc1c1d8d08f94aa7807e9b973d5e3
Clancy Bevan <cbevan@lcs.on.ca>
2021-11-19 15:28:01 -0500

    Filled in background, will add more detail later. Started on Bamboo Stalks, got 1 done. Will add more and detail later.

--- Playground.playground/Pages/Panda in Bamboo.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Panda in Bamboo.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: [Previous](@previous) / [Next](@next)
 /*:
-## Canvas size
+ ## Canvas size
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
@@ -51,68 +51,89 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
  ## Add your code
  
  Beginning on line 61, you can add your own code.
-  
+ 
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
-
+ 
  */
 
 // Begin writing your code below (you can remove the examples shown)
 
+canvas.highPerformance = true
 //Draw Ground Background
-canvas.lineColor = Color(hue: 30, saturation: 100, brightness: 29, alpha: 100)
-for DirtHeight in stride(from: -400,
-                   through: -90,
-                   by: 2){
-    canvas.drawLine(from: Point(x:-400,y:DirtHeight), to: Point(x:400, y:DirtHeight))
+canvas.lineColor = Color(hue: 30, saturation: 100, brightness: 29, alpha: 90)
+for DirtHeight in stride(from: -300,
+                         through: -90,
+                         by: 2){
+    canvas.drawLine(from: Point(x:-300,y:DirtHeight), to: Point(x:300, y:DirtHeight))
+}
+canvas.lineColor = Color(hue: 30, saturation: 100, brightness: 20, alpha: 90)
+for farDirt in stride(from: -300, through: 300, by: 2){
+    canvas.drawLine(from: Point(x:farDirt, y:-90), to: Point(x:farDirt, y:60))
 }
 
-//Draw Sky Background, Hue is either 186 or 3
-canvas.lineColor = Color(hue: 186, saturation: 100, brightness: 100, alpha: 100)
+//Draw Forest Background
+canvas.lineColor = Color(hue: 110, saturation: 64, brightness: 20, alpha: 90)
+for forestDepth in stride(from: -300, through: 300, by: 2){
+    canvas.drawLine(from: Point(x:forestDepth,y:300), to: Point(x:forestDepth, y:60))
+}
+        canvas.lineColor = Color(hue: 110, saturation: 64, brightness: 40, alpha: 90)
+        
+        for farBamboo in stride(from: -300,
+                                through: 300,
+                                by: 13.5){
+            canvas.drawLine(from: Point(x:farBamboo,y:300), to: Point(x:farBamboo, y:60))
+        }
+        
+        
+        
+        //Draw Panda Face
+        p.goto(dx: 0, dy: 0)
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 95, alpha: 100)
+        canvas.drawEllipse(at: Point(x: 0, y: 100), width: 120, height: 120, borderWidth: 5)
+        p.goToOrigin()
+        p.goto(dx: 0, dy: 40)
+        
+        //Draw Eyes
+        p.goto(dx: -20, dy: 80)
+        canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+        canvas.drawEllipse(at: Point(x: 0, y: 0), width: 20, height: 20, borderWidth: 5)
+        p.goto(dx: 40, dy: 0)
+        canvas.drawEllipse(at: Point(x: 0, y: 0), width: 20, height: 20, borderWidth: 5)
+        p.goToOrigin()
+        
+        //Draw Panda Body
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 95, alpha: 100)
+        canvas.drawEllipse(at: Point(x:0 , y:0 ), width: 80, height: 120, borderWidth: 5)
+        
+        //Draw Arms
+        canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 10, alpha: 100)
+        canvas.drawEllipse(at: Point(x:50, y:20), width: 40, height: 60, borderWidth: 5)
+        canvas.drawEllipse(at: Point(x:-50, y:20), width: 40, height: 60, borderWidth: 5)
+        
+        //Draw Legs
+        canvas.drawEllipse(at: Point(x:-20, y:-60), width: 40, height: 40, borderWidth: 5)
+        canvas.drawEllipse(at: Point(x:20, y:-60), width: 40, height: 40, borderWidth: 5)
+        
+//Draw Bamboo Stalks
+canvas.fillColor = Color(hue: 114, saturation: 92, brightness: 81, alpha: 100)
+for bamboo1body in stride(from: -160, through: 0, by: 40){
+canvas.drawEllipse(at: Point(x:-200, y:bamboo1body), width: 40, height: 100, borderWidth: 5)
+}
 
 
-//Draw Panda Face
-p.goto(dx: 0, dy: 0)
-canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 95, alpha: 100)
-canvas.drawEllipse(at: Point(x: 0, y: 100), width: 120, height: 120, borderWidth: 5)
-p.goToOrigin()
-p.goto(dx: 0, dy: 40)
-
-//Draw Eyes
-p.goto(dx: -20, dy: 80)
-canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
-canvas.drawEllipse(at: Point(x: 0, y: 0), width: 20, height: 20, borderWidth: 5)
-p.goto(dx: 40, dy: 0)
-canvas.drawEllipse(at: Point(x: 0, y: 0), width: 20, height: 20, borderWidth: 5)
-p.goToOrigin()
-
-//Draw Panda Body
-canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 95, alpha: 100)
-canvas.drawEllipse(at: Point(x:0 , y:0 ), width: 80, height: 120, borderWidth: 5)
-
-//Draw Arms
-canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 10, alpha: 100)
-canvas.drawEllipse(at: Point(x:50, y:20), width: 40, height: 60, borderWidth: 5)
-canvas.drawEllipse(at: Point(x:-50, y:20), width: 40, height: 60, borderWidth: 5)
-
-//Draw Legs
-canvas.drawEllipse(at: Point(x:-20, y:-60), width: 40, height: 40, borderWidth: 5)
-canvas.drawEllipse(at: Point(x:20, y:-60), width: 40, height: 40, borderWidth: 5)
-
-
-    
-             
-/*:
- ## Show the Live View
- Don't see any results?
- 
- Remember to show the Live View (1 then 2):
- 
- ![timeline](timeline.png "Timeline")
-
- ## Use source control
- To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source-control.png "Source Control")
- */
+        
+        /*:
+         ## Show the Live View
+         Don't see any results?
+         
+         Remember to show the Live View (1 then 2):
+         
+         ![timeline](timeline.png "Timeline")
+         
+         ## Use source control
+         To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
+         
+         Please commit and push your work often.
+         
+         ![source_control](source-control.png "Source Control")
+         */
