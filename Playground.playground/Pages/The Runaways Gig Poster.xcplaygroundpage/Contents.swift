2fa43de5830b30acaa7656f27708a0963e856eeb
Clancy Bevan <cbevan@lcs.on.ca>
2021-12-07 09:56:00 -0500

    Finished the Runaways poster, Solution was surprisingly simple. Anly need one loop as the line widths decrease at the same rate, they only have different directions.

--- Playground.playground/Pages/The Runaways Gig Poster.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/The Runaways Gig Poster.xcplaygroundpage/Contents.swift
@@ -62,42 +62,44 @@ PlaygroundPage.current.liveView = canvas
 let pink = Color(hue: 325, saturation: 100, brightness: 100, alpha: 100)
 
 //Draw Pattern
-for verticalWidth in stride(from: 400, through: 25, by: -25){
+for lineWidth in stride(from: 400, through: 25, by: -25){
     
-    if verticalWidth.isMultiple(of: 10) {
+    if lineWidth.isMultiple(of: 50) {
         canvas.lineColor = pink
-    } else if (verticalWidth + 25).isMultiple(of: 100){
+    } else if (lineWidth + 25).isMultiple(of: 100){
         canvas.lineColor = .black
-    } else if verticalWidth.isMultiple(of: 25) {
+    } else if lineWidth.isMultiple(of: 25) {
         canvas.lineColor = .white
     }
-    //        if verticalWidth == 25{
-    //            canvas.lineColor = pink
-    //        }
     
-    canvas.drawLine(from: Point(x: 200, y: 0), to: Point(x: 200, y: 400), lineWidth: verticalWidth, capStyle: .butt, dashed: false)
-}
-for horizontalWidth in stride(from: 400, through:25, by: -25){
+    canvas.drawLine(from: Point(x: 200, y: 0), to: Point(x: 200, y: 400), lineWidth: lineWidth, capStyle: .butt, dashed: false)
+    canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x: 400, y: 200), lineWidth: lineWidth, capStyle: .butt, dashed: false)
     
-    if horizontalWidth.isMultiple(of: 10) {
-        canvas.lineColor = pink
-    } else if (horizontalWidth + 25).isMultiple(of: 100){
-        canvas.lineColor = .black
-    } else if horizontalWidth.isMultiple(of: 25) {
-        canvas.lineColor = .white
-    }
-    //        if horizontalWidth == 25 {
-    //            canvas.lineColor = pink
-    //        }
-    canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x: 400, y: 200), lineWidth: horizontalWidth, capStyle: .butt, dashed: false)
 }
 
+//Upper background
+canvas.fillColor = pink
+canvas.drawRectangle(at: Point(x: 0, y: 400), width: 400, height: 200)
 
+//Date and Ticket Prices
+canvas.drawText(message: "friday", at: Point(x: 20, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "august 19 1977", at: Point(x: 20, y: 540), size: 10, kerning: 0)
+canvas.drawText(message: "tickets $4.50", at: Point(x: 20, y: 530), size: 10, kerning: 0)
+canvas.drawText(message: "friday", at: Point(x: 20, y: 550), size: 10, kerning: 0)
 
+//Special Guest Text
+canvas.drawText(message: "with", at: Point(x: 120, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "special guests", at: Point(x: 120, y: 535), size: 10, kerning: 0)
+canvas.drawText(message: "wolfgang", at: Point(x: 120, y: 520), size: 10, kerning: 0)
 
+//Location Text
+canvas.drawText(message: "at ben h. lewis hall", at: Point(x: 290, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "3443 orange street", at: Point(x: 290, y: 535), size: 10, kerning: 0)
+canvas.drawText(message: "riverside, california", at: Point(x: 290, y: 520), size: 10, kerning: 0)
 
-
-
+//Band Text
+canvas.textColor = .white
+canvas.drawText(message: "the runaways", at: Point(x: 10, y: 425), size: 55, kerning: 0)
 
 
 
